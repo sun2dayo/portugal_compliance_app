98d76d1341afc493b18c171efc11ca2d56cd4d4d
Octavio Daio <ola@novadx.eu>
2025-05-04 18:46:16 +0200

    revisao 32  app portugal_compliance

--- portugal_compliance/config/desktop.py
+++ portugal_compliance/config/desktop.py
@@ -3,17 +3,14 @@ from frappe import _
 def get_data():
 	return [
 		{
-			"module_name": "Portugal Compliance",
-			"color": "grey",
-			"icon": "octicon octicon-law",
-			"type": "module",
-			"label": _("Portugal Compliance"),
+			"label": _("Integrations"),
 			"items": [
 				{
 					"type": "doctype",
 					"name": "Portugal Compliance Settings",
-					"label": _("Settings"),
-					"description": _("Configure Portugal fiscal compliance settings.")
+					"label": _("Portugal Compliance Settings"),
+					"description": _("Configure Portugal fiscal compliance settings (SAF-T, ATCUD, Signature)."),
+					"onboard": 1,
 				}
 			]
 		}
