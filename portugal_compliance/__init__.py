733da035d3700db0efbefc3cbb5d9a160e2cc59e
Octavio Daio <ola@novadx.eu>
2025-05-04 19:35:30 +0200

    revisao 34  app portugal_compliance

--- portugal_compliance/__init__.py
+++ portugal_compliance/__init__.py
@@ -1,12 +1,2 @@
 __version__ = "1.0.0"
 
-# Explicitly import doctypes to aid discovery/search
-try:
-    from .doctype.portugal_compliance_settings.portugal_compliance_settings import PortugalComplianceSettings
-    from .doctype.document_series_pt.document_series_pt import DocumentSeriesPT
-    from .doctype.compliance_audit_log.compliance_audit_log import ComplianceAuditLog
-    from .doctype.taxonomy_code.taxonomy_code import TaxonomyCode
-except ImportError:
-    # Handle cases where doctypes might not exist during initial setup/migration phases
-    pass
-
